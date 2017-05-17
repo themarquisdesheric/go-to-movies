2e34e89cb94bb6b33a524ff21f6654a970350038
Morgan Fogarty <mofo37@gmail.com>
2017-05-17 12:59:49 -0700

    add styling, made app function.

--- go-to-movies/src/index.js
+++ go-to-movies/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import MovieApp from './Movie-app';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <MovieApp />,
   document.getElementById('root')
 );
