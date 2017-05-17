6529212e733617756240eb0b6147685fef051bd1
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-17 12:22:07 -0700

    added search functionality for other movies

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function Movie({ movie }) {
     <li>
       <h3>{ movie.Title }</h3>
       { movie.Year }
-      <img src={ movie.Poster } alt={ movie.Title } onClick={ e => console.log(e.target.src) }/>
+      <img src={ movie.Poster } 
+           alt={ movie.Title } 
+           onClick={ e => console.log('clicked!') }
+      />
     </li>
   );
 }
@@ -25,8 +28,8 @@ class App extends Component {
     this.populateMovies();
   }
 
-  populateMovies() {
-    fetch(`http://www.omdbapi.com/?s=${encodeURI('dune')}&plot=short&r=json`)
+  populateMovies(movie = 'dune') {
+    fetch(`http://www.omdbapi.com/?s=${encodeURI(movie)}&plot=short&r=json`)
       .then(res => res.json())
       .then(data => data.Search)
       .then(movies => this.setState({ movies, loading: false }));
@@ -36,12 +39,21 @@ class App extends Component {
     return (
       <div id="App">
         <h1 className="App-header">List of Attempts to Cinematize Dune</h1>
+        
+        <form onSubmit={ e => {
+          e.preventDefault();
+          this.populateMovies(e.target.elements.search.value);
+        }}>
+          <label>See attempts for a different movie:
+            <input name="search" />
+          </label>
+          <button type="submit">Search</button>
+        </form>
 
         <ul>
           { this.state.loading ? <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="loading" /> : null }
           { this.state.movies.map((movie, idx) => <Movie key={ idx } movie={ movie } /> )}
         </ul>
-
       </div>
     );
   }
