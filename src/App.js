da39848e0fe9a767ced7bb24e9e2086e2684bbdb
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-17 10:55:12 -0700

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     super();
 
     this.state = {
+      loading: true,
       movies: []
     };
   }
@@ -22,15 +23,15 @@ class App extends Component {
     fetch(`http://www.omdbapi.com/?s=${encodeURI('dune')}&plot=short&r=json`)
       .then(res => res.json())
       .then(data => data.Search)
-      .then(movies => {
-          this.setState({ movies });
-      });
+      .then(movies => this.setState({ movies, loading: false }));
   }
 
   render() {
     return (
       <div>
         <h1 className="App-header">List of Attempts to Cinematize Dune</h1>
+
+        { this.state.loading ? <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="loading" /> : null }
 
         <ul>
           { this.state.movies.map((movie, idx) => <Movie key={ idx } movie={ movie } /> )}
