b1b40f0a8a096432a9d2c78c091b7769d0453281
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-17 10:10:59 -0700

    GET movies is working, but initial didMount() is not

--- src/App.js
+++ src/App.js
@@ -2,20 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class MovieApp extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      movies: []
+    };
+  }
+
+  componenetDidMount() {
+    this.search('Indiana Jones')
+  }
+
+  search(title) {
+    this.setState({
+      loading: true,
+      movies: []
+    });
+    fetch(`http://www.omdbapi.com/?s=${encodeURI(title)}&plot=short&r=json`)
+      .then(res => res.json())
+      .then(data => data.Search)
+      .then(movies => {
+        this.setState({
+          movies,
+          loading: false
+        });
+      });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>The Ripe Banana Movie Database</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Search your favorite movies</h2>
+        {this.state.loading ? <div>Loading...</div> : null}
+        <ul>
+          {this.state.movies.map((movie, i) => <li key={i}>
+            <img src={movie.Poster} alt="Poster" />
+            {movie.Title}
+
+          </li>)}
+        </ul>
+        <div>
+          <form onSubmit={event => {
+            event.preventDefault();
+            this.search(event.target.elements.search.value);
+
+          }}>
+            <label>Title: <input name="search" /></label>
+            <button type="submit">Search</button>
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default MovieApp;
