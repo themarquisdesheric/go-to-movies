a82a7fe1b0b9425d06a3442cb0224e40fec17c2a
Ivy Patton <lilaivy@gmail.com>
2017-05-17 14:44:37 -0700

    more styling

--- go-to-movies/src/SearchResults.js
+++ go-to-movies/src/SearchResults.js
@@ -7,8 +7,8 @@ function SearchResults(props) {
             <h2 className="website_title">Search Movie Database</h2>
             {props.loading ? <div>Loading...</div> : null}
 
-            <ul>
-                {props.movies.map((movie, i) => <li key={i}>
+            <ul className="movie-list">
+                {props.movies.map((movie, i) => <li className="movie-instance" key={i}>
                     <img src={movie.Poster} />
                     <br/>
                     {movie.Title}
