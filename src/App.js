ac506d478d9c01adec83f2159e3e62da3b3f7623
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-17 10:12:16 -0700

    movies printing to screen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: []
+    };
+  }
+
+  populateMovies() {
+    fetch(`http://www.omdbapi.com/?s=${encodeURI('dune')}&plot=short&r=json`)
+      .then(res => res.json())
+      .then(data => data.Search)
+      .then(movies => {
+          this.setState({ movies });
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Let's go to the movies!</h1>
-        </div>
+      <div>
+        <h1 className="App-header">List of Attempts to Cinematize Dune</h1>
+
+        <ul>
+          { this.populateMovies() }
+          { this.state.movies.map((movie, idx) => <li key={idx}>{ movie.Title }</li> )}
+        </ul>
+
       </div>
     );
   }
